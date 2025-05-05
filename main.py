c5aa4148dd1ddf62d96d51f7e10c5ff882a94a74
louis <louis.turpault@gmail.com>
2025-05-05 14:40:54 -0400

     stuff

--- main.py
+++ main.py
@@ -1,21 +1,22 @@
-# import categorie
+from categorie import categorie
+
 # import invention
 from binarytree import Node
 
 racine = Node("Racine")
 
-def ajoute_elem(root,elem):
+
+def ajoute_elem(root, elem):
+    if type(elem) is categorie:
+        
     if not root.left:
         root.left = Node(elem)
     elif not racine.right:
         root.right = Node(elem)
     else:
-        raise ValueError(str(root)+" noeud satiété")
+        raise ValueError(str(root) + " noeud satiété")
 
-ajoute_elem(racine,2,int)
-ajoute_elem(racine,"3a",str)
+
+ajoute_elem(racine, 2, int)
+ajoute_elem(racine, "3a", str)
 print(racine)
-
-
-
-
