5192400f7932b60c571f598015823b0404205ef8
Ti-Die <ewm.hoover@gmail.com>
2025-05-05 14:25:48 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,15 +1,19 @@
-import categorie
-import invention
+# import categorie
+# import invention
 from binarytree import Node
 
-racine = Node(1)
+racine = Node("Racine")
 
-def ajoute_elem(root,elem,type):
-    if type(elem) != type:
-        raise TypeError(str(elem)+" n'est pas de classe "+str(type))
+def ajoute_elem(root,elem,type_el):
+    if type(elem) != type_el:
+        raise TypeError(str(elem)+" n'est pas de classe "+str(type_el))
     if not root.left:
         root.left = Node(elem)
     elif not racine.right:
         root.right = Node(elem)
     else:
         raise ValueError(str(root)+" noeud satiété")
+
+ajoute_elem(racine,2,int)
+ajoute_elem(racine,"3a",str)
+print(racine)
